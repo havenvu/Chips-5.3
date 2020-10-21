039d007090ec249f2ab2f8f15a09e1f9725cb506
Haven Vu <havenvu@berkeley.edu>
2020-10-21 22:08:30 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,25 @@
 class MoviesController < ApplicationController
-
+  
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+    
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = ['G','PG','PG-13','R']
+    if params[:ratings].nil?
+      @ratings_to_show = []
+    else
+      @ratings_to_show = params[:ratings].keys
+    end
+    @movies = Movie.with_ratings(@ratings_to_show)
+
+#     byebug
+
+
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,5 +3,13 @@ class Movie < ActiveRecord::Base
   # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
   #  movies with those ratings
   # if ratings_list is nil, retrieve ALL movies
+    if ratings_list == []
+      return Movie.all
+    elsif ratings_list.nil?
+      return Movie.all
+    else
+      Movie.where(:rating => ratings_list)
+    end
   end
+
 end
